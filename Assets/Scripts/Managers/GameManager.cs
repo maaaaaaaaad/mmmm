1951f3e9fe582a95edd7f0c98a7b479819b879d0
maaaaaaaaad <integral5137@gmail.com>
2026-01-09 09:36:42 +0900

    .

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+namespace Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        private const string ObjName = "@Manager";
+        private static GameManager _instance;
+        public static GameManager GetInstance() => _instance;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Init()
+        {
+            if (_instance != null)
+                return;
+
+            var go = GameObject.Find(ObjName);
+            if (go == null)
+            {
+                go = new GameObject { name = ObjName };
+                _instance = go.AddComponent<GameManager>();
+            }
+            else
+            {
+                _instance = go.GetComponent<GameManager>();
+                DontDestroyOnLoad(go);
+            }
+        }
+
+        private void Update()
+        {
+        }
+
+        private void OnDestroy()
+        {
+            if (_instance == this)
+                _instance = null;
+        }
+    }
+}
